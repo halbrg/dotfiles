e0f76a93d150beb6e7878587bef9df2d9f845570
Oskar Ahlberg <59509735+halbrg@users.noreply.github.com>
2024-10-13 20:10:02 +0200

    Add bufferline.nvim to nvim config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,5 +3,6 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {}
   }
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -3,5 +3,20 @@ return {
     "folke/trouble.nvim",
     opts = {},
     cmd = "Trouble"
+  },
+
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "catppuccin"
+    },
+    opts = {
+        options = {
+            separator_style = "slant"
+        },
+        highlights = require("catppuccin.groups.integrations.bufferline").get()
+    }
   }
 }
