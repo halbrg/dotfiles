8b2b30045811a75bd1d68c129f275f688bbaf783
Oskar Ahlberg <59509735+halbrg@users.noreply.github.com>
2024-10-14 11:23:37 +0200

    Merge LSP auto-setup from nemesis

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,20 @@
 return {
-    { "neovim/nvim-lspconfig" },
-
     { "mrcjkb/rustaceanvim" },
+    { "mfussenegger/nvim-jdtls" },
 
-    { "mfussenegger/nvim-jdtls" }
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            local mason_lspconfig = require("mason-lspconfig")
+
+            mason_lspconfig.setup_handlers({
+                function(server)
+                    lspconfig[server].setup({})
+                end,
+                ["rust_analyzer"] = function() end,
+                ["jdtls"] = function() end
+            })
+        end
+    },
 }
